8466a7be250c61aad06a71ce5cb13f6cee50f51c
BBQGiraffe <alexanderortizbusiness@gmail.com>
2022-01-30 13:58:33 -0600

    replace vincent with Pluto

---
+++ Bullet.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zooiiscool
+{
+    enum SenderType
+    {
+
+        player,
+        badguy
+    };
+
+    class Bullet : Entity
+    {
+        float speed = 420;
+        float time = 0f;
+        float maxtime = 20;
+        float direction = 0f;
+        public override void Start()
+        {
+            texture = "pluto.png";
+        }
+        
+        public SenderType type;
+
+        public Bullet(float speed, float direction, SenderType type)
+        {
+            this.speed = speed;
+            this.type = type;
+            this.direction = direction;
+        }
+        public override void Update()
+        {
+            time += Time.DeltaTime;
+            if(time >= maxtime)
+            {
+                active = false;
+            }
+
+            position.X += direction*speed * Time.DeltaTime;
+        }
+    }
+}
